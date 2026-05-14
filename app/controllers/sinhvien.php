25970d41fa0d1d3cd3afbcc109a854c65e805088
trunhhieu <trunhhieu2k5@gmail.com>
2026-05-14 22:06:02 +0700

    URL Process

---
+++ app/controllers/sinhvien.php
@@ -0,0 +1,9 @@
+<?php
+class sinhvien{
+    public function index(){
+        echo "Đây là trang sinh viên";
+    }
+    public function create(){
+        echo "Đây là trang tạo sinh viên";
+    }
+}
