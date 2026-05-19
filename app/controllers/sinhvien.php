fad826244edecdf2e1110af83561cf90d37b8c83
trunhhieu <trunhhieu2k5@gmail.com>
2026-05-19 22:44:14 +0700

    Delete app directory

--- app/controllers/sinhvien.php
+++
@@ -1,9 +0,0 @@
-<?php
-class sinhvien{
-    public function index(){
-        echo "Đây là trang sinh viên";
-    }
-    public function create(){
-        echo "Đây là trang tạo sinh viên";
-    }
-}
